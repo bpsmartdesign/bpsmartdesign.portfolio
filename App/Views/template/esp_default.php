b91de08e54ef338622e7699930b48f12c02f0269
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2021-05-26 13:27:34 +0200

    add messenger pluging

--- App/Views/template/esp_default.php
+++ App/Views/template/esp_default.php
@@ -338,6 +338,32 @@
 			</div>
 		</footer>
 	</div>
+	<!-- Messenger Chat plugin Code -->
+		<div id="fb-root"></div>
+
+	<!-- Your Chat plugin code -->
+	<div id="fb-customer-chat" class="fb-customerchat">
+	</div>
+
+	<script>
+		var chatbox = document.getElementById('fb-customer-chat');
+		chatbox.setAttribute("page_id", "104562031627785");
+		chatbox.setAttribute("attribution", "biz_inbox");
+		window.fbAsyncInit = function() {
+			FB.init({
+				xfbml            : true,
+				version          : 'v10.0'
+			});
+		};
+
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
 	<script type="text/javascript" src="Dist/js/anime.min.js"></script>
 	<script type="text/javascript" src="Dist/js/dancing_word.js"></script>
 	<script type="text/javascript" src="Dist/js/active_menu.js"></script>
